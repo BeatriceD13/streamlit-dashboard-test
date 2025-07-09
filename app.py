285b45dd1bc10ebe31f79178a29a2134476ca41b
BeatriceD13 <b.desrochers@bluelineinc.com>
2025-07-09 11:02:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,52 @@ import matplotlib.pyplot as plt
 import streamlit_authenticator as stauth
 
 st.set_page_config(page_title="Dashboard Matplotlib", layout="centered")
-st.title("Exemple de Dashboard avec Matplotlib")
 
-# Données
-df = pd.DataFrame({
-    "Catégorie": ["A", "B", "C", "D"],
-    "Valeur": [10, 23, 7, 19]
-})
+# --- Authentification ---
+hashed_passwords_bea = 'TestBlueline2026$'
+hashed_passwords_dave = 'TestBlueline2026$'
 
-# Graphique
-fig, ax = plt.subplots()
-ax.bar(df["Catégorie"], df["Valeur"])
-ax.set_title("Valeur par Catégorie")
 
-st.pyplot(fig)
+users = {
+    "beatrice": "Beatrice Desrochers",
+    "dave": "Dave Murray"
+}
 
-# Affichage des données si coché
-if st.checkbox("Afficher les données brutes"):
-    st.dataframe(df)
+passwords = {
+    "beatrice": hashed_passwords_bea,  # hash de Beatrice
+    "dave": hashed_passwords_dave      # hash de Jean
+}
+authenticator = stauth.Authenticate(
+    users,
+    passwords,
+    "app_dashboard",
+    "abcdef",  # clé secrète
+    cookie_expiry_days=1
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    st.title(f"Bienvenue {name} !")
+    
+    # Ton dashboard ici :
+    df = pd.DataFrame({
+        "Catégorie": ["A", "B", "C", "D"],
+        "Valeur": [10, 23, 7, 19]
+    })
+    
+    fig, ax = plt.subplots()
+    ax.bar(df["Catégorie"], df["Valeur"])
+    ax.set_title("Valeur par Catégorie")
+    
+    st.pyplot(fig)
+    
+    if st.checkbox("Afficher les données brutes"):
+        st.dataframe(df)
+    
+    authenticator.logout('Logout', 'sidebar')
+
+elif authentication_status is False:
+    st.error("Nom d'utilisateur ou mot de passe incorrect")
+else:
+    st.info("Veuillez vous connecter")
